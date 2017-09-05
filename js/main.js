86bdd9afff73cf3b460b97dad53ad462f415c188
Julie Bernal <juliebernal@julies-mbp.home>
2017-09-05 18:44:48 +0200

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 console.log("Up and running!");
-
-var cardOne = "queen";
-var cardTwo = "king";
-var cardoneThree = "queen";
-var cardFour = "king";
 console.log("User flipped " + cardOne);
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay =[];
+cardsInPlay.push ("cards[cardId]");
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay){
+		console.log ("you found a match");
+} elese {
+	console.log ("sorry, try again");
+}
+};
+var cardOne = CardsinPlay[0];
+var flipCard = function(cardId){
+
+console.log("User flipped queen"+ cards{cardId});
+fliCard(0);
+fliCard(2);
+checkForMatch();
+
+};
