c861b9adaeaf29a34e892bd15fd085eaaf221c54
Julie Bernal <juliebernal@julies-macbook-pro.home>
2017-09-12 16:20:06 +0200

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push("queen");
+console.log ("user flipped" + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push("king");
+console.log ("user flipped" + cardTwo);
+ if (cardsInPlay.length === 2) {;
+
+} else if  (cardsInPlay[0] === cardsInPlay[1]) {
+	alert ("you found a match!");
+}
+	else {
+	alert ("sorry, try again!");
+}
+
+
+
 
